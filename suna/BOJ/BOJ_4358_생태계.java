015760940ffd1ac57bfcc959228b0ad901d131e6
DdatGe <109500474+DdatGe@users.noreply.github.com>
2026-03-15 15:39:21 +0900

    20260315 - scanner 추가
    Commented out the main method and related code for scanner usage.

--- suna/BOJ/BOJ_4358_생태계.java
+++ suna/BOJ/BOJ_4358_생태계.java
@@ -30,8 +30,32 @@ public class Main {
 
 	}
 }
-//		
+
+// scanner 사용하는 경우
+ain {
+//	public static void main(String[] args) throws IOException {
+//		Scanner sc = new Scanner(System.in);
+//		TreeMap<String, Double> tm = new TreeMap<>();
+//		int cnt = 0;
+//		while (sc.hasNextLine()) { // scanner로 처리하려면 isEmpty()로 체크 
+//			String input = sc.nextLine();
+//			if (input.isEmpty()) break;
+//			if (!tm.containsKey(input)) {
+//				tm.put(input, (double) 1);
+//				cnt++;
+//			} else {
+//				double tmp = tm.get(input);
+//				tm.remove(input);
+//				tm.put(input, tmp + 1);
+//				cnt++;
+//			}
+//		}
+//		StringBuilder sb = new StringBuilder();
+//		for (String str : tm.keySet()) {
+//			sb.append(str).append(" ").append(String.format("%.4f\n", (tm.get(str) / cnt * 100)));
+//		}
+//
 //		System.out.println(sb);
-//		
+//
 //	}
 //}
