65c3de21c69b0cb20555d4dad03b7a29ab83d1da
DdatGe <109500474+DdatGe@users.noreply.github.com>
2026-03-15 15:33:46 +0900

    20260315

---
+++ suna/BOJ/BOJ_4358_생태계.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.TreeMap;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		TreeMap<String, Double> tm = new TreeMap<>();
+		int cnt = 0;
+		String input = br.readLine();
+		while (input != null && !input.isEmpty()) { // input!= null을 해야 널포인트 에러가 안난다. 
+			if (!tm.containsKey(input)) {
+				tm.put(input, (double) 1);
+				cnt++;
+			} else {
+				double tmp = tm.get(input);
+				tm.remove(input);
+				tm.put(input, tmp + 1);
+				cnt++;
+			}
+			input = br.readLine();
+		}
+		StringBuilder sb = new StringBuilder();
+		for (String str : tm.keySet()) {
+			sb.append(str).append(" ").append(String.format("%.4f\n", (tm.get(str) / cnt * 100)));
+		}
+
+		System.out.println(sb);
+
+	}
+}
+//		
+//		System.out.println(sb);
+//		
+//	}
+//}
